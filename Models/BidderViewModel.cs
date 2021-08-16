ce136fd76c9478f34e21097e8f5d71e856e3d6cf
Ak47u1999 <arpittopper.2017@gmail.com>
2021-08-16 18:18:35 +0530

    .

---
+++ Models/BidderViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmerScheme.Models
+{
+    public class BidderViewModel
+    {
+        public BidderViewModel(int requestId, string bidderName, decimal? bidAmount, DateTime? bidDate, bool? bidAdminApprovalStatus)
+        {
+            this.requestId = requestId;
+            this.bidderName = bidderName;
+            BidAmount = bidAmount;
+            BidDate = bidDate;
+            BidAdminApprovalStatus = bidAdminApprovalStatus;
+        }
+
+        public BidderViewModel() { }
+
+        public int? requestId { get; set; }
+        public string bidderName { get; set; }
+        public decimal? BidAmount { get; set; }
+        public DateTime? BidDate { get; set; }
+        public bool? BidAdminApprovalStatus { get; set; }
+    }
+}
